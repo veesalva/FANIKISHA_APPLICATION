f44053b464c0e75326f1a9f71cee1efcd1d4c45e
sandy_andrew <133486852+Proaviator@users.noreply.github.com>
2023-08-31 14:45:35 +0300

    Added date input SizedBox

--- lib/screens/CreateGoal.dart
+++ lib/screens/CreateGoal.dart
@@ -13,6 +13,7 @@ class CreateGoal extends StatefulWidget {
 
 class _CreateGoalState extends State<CreateGoal> {
   GoalPriorityEnum? _goalPriorityEnum;
+  final TextEditingController _date = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -37,181 +38,245 @@ class _CreateGoalState extends State<CreateGoal> {
           ],
         ),
       ),
-      body: ListView(children: [
-        const SizedBox(
-          height: 15,
-        ),
-        Container(
-          margin: const EdgeInsets.all(10),
-          child: const Text(
-            "Goal Description",
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+      body: ListView(
+        children: [
+          const SizedBox(
+            height: 15,
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Form(
-              child: Column(
+          Container(
+            margin: const EdgeInsets.all(10),
+            child: const Text(
+              "Goal Description",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Form(
+                child: Column(
+              children: [
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: "Goal Name",
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: "Goal Type",
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: "Amount",
+                    border: OutlineInputBorder(),
+                  ),
+                  keyboardType: TextInputType.number,
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: "Payment Number",
+                    border: OutlineInputBorder(),
+                  ),
+                  keyboardType: TextInputType.number,
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+              ],
+            )),
+          ),
+          Container(
+            margin: const EdgeInsets.all(15),
+            child: const Text(
+              "Goal Priority",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Row(
             children: [
-              TextFormField(
-                decoration: const InputDecoration(
-                  labelText: "Goal Name",
-                  border: OutlineInputBorder(),
-                ),
+              Expanded(
+                child: RadioListTile<GoalPriorityEnum>(
+                    hoverColor: Colors.green,
+                    activeColor: Colors.green,
+                    contentPadding: const EdgeInsets.only(left: 20.0),
+                    value: GoalPriorityEnum.High,
+                    groupValue: _goalPriorityEnum,
+                    dense: true,
+                    title: Text(
+                      GoalPriorityEnum.High.name,
+                      style: const TextStyle(fontSize: 15),
+                    ),
+                    onChanged: (val) {
+                      setState(() {
+                        _goalPriorityEnum = val;
+                      });
+                    }),
               ),
-              const SizedBox(
-                height: 15,
+              Expanded(
+                child: RadioListTile<GoalPriorityEnum>(
+                    hoverColor: Colors.green,
+                    activeColor: Colors.green,
+                    contentPadding: const EdgeInsets.only(left: 4.0),
+                    value: GoalPriorityEnum.Medium,
+                    groupValue: _goalPriorityEnum,
+                    dense: true,
+                    title: Text(
+                      GoalPriorityEnum.Medium.name,
+                      style: const TextStyle(fontSize: 15),
+                    ),
+                    onChanged: (val) {
+                      setState(() {
+                        _goalPriorityEnum = val;
+                      });
+                    }),
               ),
-              TextFormField(
-                decoration: const InputDecoration(
-                  labelText: "Goal Type",
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              const SizedBox(
-                height: 15,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(
-                  labelText: "Amount",
-                  border: OutlineInputBorder(),
-                ),
-                keyboardType: TextInputType.number,
-              ),
-              const SizedBox(
-                height: 15,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(
-                  labelText: "Payment Number",
-                  border: OutlineInputBorder(),
-                ),
-                keyboardType: TextInputType.number,
-              ),
-              const SizedBox(
-                height: 5,
-              ),
-            ],
-          )),
-        ),
-        Container(
-          margin: const EdgeInsets.all(15),
-          child: const Text(
-            "Goal Priority",
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          ),
-        ),
-        Row(
-          children: [
-            Expanded(
-              child: RadioListTile<GoalPriorityEnum>(
-                  hoverColor: Colors.green,
-                  activeColor: Colors.green,
-                  contentPadding: const EdgeInsets.only(left: 20.0),
-                  value: GoalPriorityEnum.High,
-                  groupValue: _goalPriorityEnum,
-                  dense: true,
-                  title: Text(
-                    GoalPriorityEnum.High.name,
-                    style: const TextStyle(fontSize: 15),
-                  ),
-                  onChanged: (val) {
-                    setState(() {
-                      _goalPriorityEnum = val;
-                    });
-                  }),
-            ),
-            Expanded(
-              child: RadioListTile<GoalPriorityEnum>(
-                  hoverColor: Colors.green,
-                  activeColor: Colors.green,
-                  contentPadding: const EdgeInsets.only(left: 4.0),
-                  value: GoalPriorityEnum.Medium,
-                  groupValue: _goalPriorityEnum,
-                  dense: true,
-                  title: Text(
-                    GoalPriorityEnum.Medium.name,
-                    style: const TextStyle(fontSize: 15),
-                  ),
-                  onChanged: (val) {
-                    setState(() {
-                      _goalPriorityEnum = val;
-                    });
-                  }),
-            ),
-            Expanded(
-              child: RadioListTile<GoalPriorityEnum>(
-                  hoverColor: Colors.green,
-                  activeColor: Colors.green,
-                  contentPadding: const EdgeInsets.only(left: 2.0),
-                  value: GoalPriorityEnum.Low,
-                  groupValue: _goalPriorityEnum,
-                  dense: true,
-                  title: Text(
-                    GoalPriorityEnum.Low.name,
-                    style: const TextStyle(fontSize: 15),
-                  ),
-                  onChanged: (val) {
-                    setState(() {
-                      _goalPriorityEnum = val;
-                    });
-                  }),
-            ),
-          ],
-        ),
-        Container(
-          margin: const EdgeInsets.all(12),
-          child: const Text(
-            "Goal Duration",
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          ),
-        ),
-        const Padding(
-          padding: EdgeInsets.all(12.0),
-          child: Row(
-            children: [
-              Column(
-                children: [
-                  Text(
-                    'From:',
-                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(height: 20),
-                ],
-              ),
-              SizedBox(width: 180),
-              Column(
-                children: [
-                  Text(
-                    'To:',
-                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(height: 3),
-                ],
+              Expanded(
+                child: RadioListTile<GoalPriorityEnum>(
+                    hoverColor: Colors.green,
+                    activeColor: Colors.green,
+                    contentPadding: const EdgeInsets.only(left: 2.0),
+                    value: GoalPriorityEnum.Low,
+                    groupValue: _goalPriorityEnum,
+                    dense: true,
+                    title: Text(
+                      GoalPriorityEnum.Low.name,
+                      style: const TextStyle(fontSize: 15),
+                    ),
+                    onChanged: (val) {
+                      setState(() {
+                        _goalPriorityEnum = val;
+                      });
+                    }),
               ),
             ],
           ),
-        ),
-        const SizedBox(
-          height: 20,
-        ),
-        Padding(
-          padding: const EdgeInsets.all(60.0),
-          child: SizedBox(
-            width: 26,
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const SavingPlanOptions(),
-                    ));
-              },
-              child: const Text("ADD GOAL"),
+          Container(
+            margin: const EdgeInsets.all(12),
+            child: const Text(
+              "Goal Duration",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
           ),
-        )
-      ]),
+          const Padding(
+            padding: EdgeInsets.all(15.0),
+            child: Row(
+              children: [
+                Column(
+                  children: [
+                    Text(
+                      'From:',
+                      style:
+                          TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+                SizedBox(width: 180),
+                Column(
+                  children: [
+                    Text(
+                      'To:',
+                      style:
+                          TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SizedBox(
+                  width: 120,
+                  height: 40,
+                  child: TextFormField(
+                      controller: _date,
+                      decoration: const InputDecoration(
+                        labelText: "Start Date",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(8)),
+                        ),
+                      ),
+                      keyboardType: TextInputType.datetime,
+                      onTap: () async {
+                        DateTime? pickeddate = await showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime(2000),
+                          lastDate: DateTime(2100),
+                        );
+                        setState(() {
+                          _date.text =
+                              DateFormat('yyyy-MM-dd').format(pickeddate!);
+                        });
+                      }),
+                ),
+              ),
+              const SizedBox(
+                  width: 100),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SizedBox(
+                  width: 120,
+                  height: 40,
+                  child: TextFormField(
+                      controller: _date,
+                      decoration: const InputDecoration(
+                        labelText: "End Date",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(8)),
+                        ),
+                      ),
+                      keyboardType: TextInputType.datetime,
+                      onTap: () async {
+                        DateTime? pickeddate = await showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime(2000),
+                          lastDate: DateTime(2100),
+                        );
+                        setState(() {
+                          _date.text =
+                              DateFormat('yyyy-MM-dd').format(pickeddate!);
+                        });
+                      }),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 4,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(60.0),
+            child: SizedBox(
+              width: 26,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const SavingPlanOptions(),
+                      ));
+                },
+                child: const Text("ADD GOAL"),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -131,7 +131,7 @@ class HomePage extends StatelessWidget {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder: (context) => SignUp(),
+                        builder: (context) => CreateGoal(),
                       ),
                     );
                   },
