ab9fcb9086cf2ea7574c2242ec8897ede0ffdb1b
sandy_andrew <133486852+Proaviator@users.noreply.github.com>
2023-09-01 13:00:35 +0300

    Minor changes on signup page

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -131,7 +131,7 @@ class HomePage extends StatelessWidget {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder: (context) => CreateGoal(),
+                        builder: (context) => SignUp(),
                       ),
                     );
                   },
