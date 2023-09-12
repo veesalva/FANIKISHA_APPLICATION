17265cfd0637dbd867a23cfbb5f85da56e8e0c42
kichekocultoon <kichekoupendo12@gmail.com>
2023-09-12 09:07:15 +0300

    Update Constant.dart

--- lib/constant/Constant.dart
+++ lib/constant/Constant.dart
@@ -2,6 +2,6 @@
 class Constant{
 
   // for connecting phsical device and the localhost
-  static const String ipAddress="192.168.137.1";
+  static const String ipAddress="192.168.1.132";
   static const String authToken="authToken";
 }
